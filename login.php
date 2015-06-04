7022064b7997b086f2ff982e00eed273ad2f2b26
champ11101 <wchalit@hotmail.com>
2015-06-04 16:41:43 +0700

    ปรับแต่งหน้า login เล็กน้อย
    ทำอะไรไปบ้างจำไม่ได้

--- login.php
+++ login.php
@@ -2,21 +2,22 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Login</title>
+<title>Champ Login</title>
 </head>
 
 <body>
+<div align="right" ><font size="+3" >Champ</font></div>
 <form id="form1" name="form1" method="post" action="check_login.php">
   <p align="center">Login</p>
   <div align="center">
-    <table width="200" border="1">
+    <table width="261" border="1">
       <tr>
-        <td>Username</td>
-        <td><label for="textfield"></label>
+        <td width="101"><div align="center">Username</div></td>
+        <td width="144"><label for="textfield"></label>
         <input type="text" name="username" id="username" placeholder="Enter Username" /></td>
       </tr>
       <tr>
-        <td>Password</td>
+        <td><div align="center">Password</div></td>
         <td><label for="textfield2"></label>
         <input type="password" name="password" id="password" placeholder="Enter Password" /></td>
       </tr>
