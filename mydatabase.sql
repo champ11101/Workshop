a0682fe99a13e72b860d696ec17bbdb65991be1c
champ11101 <wchalit@hotmail.com>
2015-06-04 16:35:14 +0700

    เพิ่มและแก้ไขฐานข้อมูล
    ในตาราง member เพิ่ม column IP address ของเครื่องตัวเอง ;
    เพิ่มตาราง message_offline สำหรับพักข้อความก่อนส่ง ;
    เพิ่มตาราง friends เพื่อใช้เปรียบเทียบ username และ IP adress ;
    เพิ่มตาราง incoming_message สำหรับรับข้อความเข้าและแสดงบนหน้าจอ ;

--- mydatabase.sql
+++ mydatabase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2015 at 11:21 AM
+-- Generation Time: Jun 04, 2015 at 11:29 AM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -23,6 +23,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `friends`
+--
+
+CREATE TABLE IF NOT EXISTS `friends` (
+  `username` varchar(20) NOT NULL,
+  `IP_address` varchar(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `incoming_message`
+--
+
+CREATE TABLE IF NOT EXISTS `incoming_message` (
+  `sender_username` varchar(20) NOT NULL,
+  `message` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `member`
 --
 
@@ -31,26 +53,51 @@ CREATE TABLE IF NOT EXISTS `member` (
   `Username` varchar(20) NOT NULL,
   `Password` varchar(20) NOT NULL,
   `Name` varchar(50) NOT NULL,
-  `Status` enum('ADMIN','USER') NOT NULL DEFAULT 'USER'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+  `Status` enum('ADMIN','USER') NOT NULL DEFAULT 'USER',
+  `IP_address` varchar(12) NOT NULL DEFAULT '10.70.77.161'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `member`
 --
 
-INSERT INTO `member` (`UserID`, `Username`, `Password`, `Name`, `Status`) VALUES
-(001, 'tony', 'tony123', 'tony stark', 'USER'),
-(002, 'james', 'james123', 'james cameron', 'USER');
+INSERT INTO `member` (`UserID`, `Username`, `Password`, `Name`, `Status`, `IP_address`) VALUES
+(001, 'champ11101', '1234', 'Chalit Wangvorapinyo', 'USER', '10.70.77.161');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `message_offline`
+--
+
+CREATE TABLE IF NOT EXISTS `message_offline` (
+  `No.` int(3) unsigned zerofill NOT NULL,
+  `send_to` varchar(20) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `from_IP` varchar(12) NOT NULL DEFAULT '10.70.77.161'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`username`);
+
+--
 -- Indexes for table `member`
 --
 ALTER TABLE `member`
   ADD PRIMARY KEY (`UserID`), ADD UNIQUE KEY `Username` (`Username`);
+
+--
+-- Indexes for table `message_offline`
+--
+ALTER TABLE `message_offline`
+  ADD PRIMARY KEY (`No.`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -60,7 +107,12 @@ ALTER TABLE `member`
 -- AUTO_INCREMENT for table `member`
 --
 ALTER TABLE `member`
-  MODIFY `UserID` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `UserID` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+--
+-- AUTO_INCREMENT for table `message_offline`
+--
+ALTER TABLE `message_offline`
+  MODIFY `No.` int(3) unsigned zerofill NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
